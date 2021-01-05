52160036d2b59e02063395dfce3de592f900c352
Hoon Hur <hoon@Hoons-MacBook-Pro.local>
2021-01-05 00:26:07 -0500

    edit main.js

--- main.js
+++ main.js
@@ -35,30 +35,6 @@ var app = http.createServer(function (request, response) {
         response.end(html);
       });
     } else {
-      // fs.readdir("./data", function (error, filelist) {
-      //   var filteredId = path.parse(queryData.id).base;
-      //   fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-      //     var title = queryData.id;
-      //     var sanitizedTitle = sanitizeHtml(title);
-      //     var sanitizedDescription = sanitizeHtml(description, {
-      //       allowedTags: ["h1"],
-      //     });
-      //     var list = template.list(filelist);
-      //     var html = template.HTML(
-      //       sanitizedTitle,
-      //       list,
-      //       `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      //       ` <a href="/create">create</a>
-      //           <a href="/update?id=${sanitizedTitle}">update</a>
-      //           <form action="delete_process" method="post">
-      //             <input type="hidden" name="id" value="${sanitizedTitle}">
-      //             <input type="submit" value="delete">
-      //           </form>`
-      //     );
-      //     response.writeHead(200);
-      //     response.end(html);
-      //   });
-      // });
       db.query(`SELECT * FROM topic`, (err, topics) => {
         if (err) throw err;
         db.query(
