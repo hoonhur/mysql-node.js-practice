9e131853ae8ba7d7c3d9f568d3be6f68178cdde6
Hoon Hur <hoon@hoons-mbp.hotspot>
2021-01-09 11:47:41 -0500

    update main.js using topic module

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 var http = require("http");
 var url = require("url");
-var qs = require("querystring");
-var template = require("./lib/template.js");
-var mysql = require("mysql");
-var db = mysql.createConnection({
-  host: "localhost",
-  user: "nodejs",
-  password: "11111111",
-  database: "opentutorials",
-});
-db.connect();
+const topic = require("./lib/topic");
 
 var app = http.createServer((req, res) => {
   var _url = req.url;
@@ -17,161 +8,20 @@ var app = http.createServer((req, res) => {
   var pathname = url.parse(_url, true).pathname;
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      db.query(`SELECT * FROM topic`, (err, topics) => {
-        if (err) throw err;
-        var title = "Welcome";
-        var description = "Hello, Node.js";
-        var list = template.list(topics);
-        var html = template.HTML(
-          title,
-          list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        res.writeHead(200);
-        res.end(html);
-      });
+      topic.home(req, res);
     } else {
-      db.query(`SELECT * FROM topic`, (err, topics) => {
-        if (err) throw err;
-        db.query(
-          `SELECT topic.id, topic.title, topic.description, topic.created, topic.author_id, author.name, author.profile FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,
-          [queryData.id],
-          (err2, topic) => {
-            if (err2) throw err2;
-            var title = topic[0].title;
-            var list = template.list(topics);
-            var html = template.HTML(
-              title,
-              list,
-              `<h2>${title}</h2>
-              ${topic[0].description}
-              <p>by ${topic[0].name}</p>
-              `,
-              `<a href="/create">create</a>
-                <a href="/update?id=${topic[0].id}">update</a>
-                <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${topic[0].id}">
-                <input type="submit" value="delete">
-                </form>`
-            );
-            res.writeHead(200);
-            res.end(html);
-          }
-        );
-      });
+      topic.page(req, res);
     }
   } else if (pathname === "/create") {
-    db.query(`SELECT * FROM topic`, (err, topics) => {
-      if (err) throw err;
-      db.query("SELECT * FROM author", (err2, authors) => {
-        if (err2) throw err2;
-        var title = "create";
-        var list = template.list(topics);
-        var html = template.HTML(
-          title,
-          list,
-          `<form action="/create_process" method="post">
-              <p><input type="text" name="title" placeholder="title"></p>
-              <p>
-                <textarea name="description" placeholder="description"></textarea>
-              </p>
-              <p>
-               ${template.authorSelect(authors)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-          `,
-          ``
-        );
-        res.writeHead(200);
-        res.end(html);
-      });
-    });
+    topic.create(req, res);
   } else if (pathname === "/create_process") {
-    var body = "";
-    req.on("data", (data) => (body = body + data));
-    req.on("end", () => {
-      var post = qs.parse(body);
-      db.query(
-        `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
-        [post.title, post.description, post.author],
-        (err, result) => {
-          if (err) throw err;
-          res.writeHead(302, { Location: `/?id=${result.insertId}` });
-          res.end();
-        }
-      );
-    });
+    topic.create_process(req, res);
   } else if (pathname === "/update") {
-    db.query("SELECT * FROM topic", (err, topics) => {
-      if (err) throw err;
-      db.query(
-        `SELECT * FROM topic WHERE id=?`,
-        [queryData.id],
-        (err2, topic) => {
-          if (err2) throw err2;
-          db.query("SELECT * FROM author", (err3, authors) => {
-            if (err3) throw err3;
-            var list = template.list(topics);
-            var html = template.HTML(
-              topic[0].title,
-              list,
-              `<form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${
-                topic[0].title
-              }"></p>
-              <p>
-                <textarea name="description" placeholder="description">${
-                  topic[0].description
-                }</textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors, topic[0].author_id)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-              </form>`,
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-            res.writeHead(200);
-            res.end(html);
-          });
-        }
-      );
-    });
+    topic.update(req, res);
   } else if (pathname === "/update_process") {
-    var body = "";
-    req.on("data", (data) => (body = body + data));
-    req.on("end", () => {
-      var post = qs.parse(body);
-      db.query(
-        `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
-        [post.title, post.description, post.author, post.id],
-        (err, result) => {
-          if (err) throw err;
-          res.writeHead(302, { Location: `/?id=${post.id}` });
-          res.end();
-        }
-      );
-    });
+    topic.update_process(req, res);
   } else if (pathname === "/delete_process") {
-    var body = "";
-    req.on("data", (data) => {
-      body = body + data;
-    });
-    req.on("end", () => {
-      var post = qs.parse(body);
-      db.query(`DELETE FROM topic WHERE id=?`, [post.id], (err, result) => {
-        if (err) throw err;
-        res.writeHead(302, { Location: `/` });
-        res.end();
-      });
-    });
+    topic.delete_process(req, res);
   } else {
     res.writeHead(404);
     res.end("Not found");
